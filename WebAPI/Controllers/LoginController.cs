66dd23f01a60029d61cc7b93e0a0ad3da0c9f05a
zap123-456 <1810766198@qq.com>
2022-10-01 23:57:42 +0800

    1

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Dto.User;
+using Model.Other;
+using WebAPI.Config;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUserService _UserService;
+        private ICustomJWTService _CustomJWTService;
+
+        public LoginController(IUserService UserService, ICustomJWTService CustomJWTService) { 
+            _UserService = UserService;
+            _CustomJWTService = CustomJWTService;
+        }
+        [HttpGet]
+        public async Task<ApiResult> GetToken(string name,string password) {
+            var res = Task.Run(() => {
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password)) {
+                    return ResultHelper.Error("参数不能为空");
+                }
+                UserRes user = _UserService.GetUser(name, password);
+                if (string.IsNullOrEmpty(user.Name)) {
+                    return ResultHelper.Error("账号密码错误");
+                }
+                return ResultHelper.Success(_CustomJWTService.GetToken(user));
+            });
+            return await res;
+        }
+    }
+}
